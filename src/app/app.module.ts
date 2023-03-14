dc5e03788c88f8ee27a7c1225298731591cac03b
Audrey <afacquez@riviera-networks.com>
2023-03-14 12:26:52 +0100

    css alert box and refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from "./shared/services/auth-interceptor.service";
-import { AlertComponent } from './shared/components/alert/alert/alert.component';
+import { AlertComponent } from './shared/components/alert/alert.component';
 
 @NgModule({
   declarations: [
