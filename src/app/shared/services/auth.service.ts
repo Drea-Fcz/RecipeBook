3d5cd681d0f386bac01e7facd8e60188d7663c5c
Audrey <afacquez@riviera-networks.com>
2023-03-14 10:01:13 +0100

    autoLogout & clear local storage after logout

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -20,6 +20,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user = new BehaviorSubject<UserModel>(null);
+  private tokenExpirationTimer: any;
   constructor(private http: HttpClient, private router: Router) { }
 
   singUp(email: string, password: string): any {
@@ -75,16 +76,34 @@ export class AuthService {
     );
     if (loadedUser.token) {
       this.user.next(loadedUser);
+      const expirationDuration =  new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration)
     }
   }
 
   logout(): void {
     this.user.next(null);
     this.router.navigate( ['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
   }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number )
-  {
+  autoLogout(expirationDuration: number): void{
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    },expirationDuration)
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
     const expirationDate = new Date(new Date().getTime() +  expiresIn * 1000);
     const user =  new UserModel(
       email,
@@ -93,6 +112,7 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    this.autoLogout(expiresIn);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
